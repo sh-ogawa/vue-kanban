9f1db6eaabf2578792c429b9ba5e137170d1e5ef
sh-ogawa <syogo0417@gmail.com>
2018-10-25 23:24:24 +0900

    add KbnButton unit-test.

---
+++ src/components/atoms/KbnButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <button
+    :class="classes"
+    :disabled="disabled"
+    :type="button"
+    @click="handleClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'KbnButton',
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    classes () {
+      const cls = this.type === 'text' ? ('-' + this.type) : ''
+      return [`kbn-button${cls}`]
+    }
+  },
+  methods: {
+    handleClick (ev) {
+      this.$emit('click', ev)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .kb-button {
+    padding: .6em 1.3em;
+  }
+  .kbn-button-text {
+    border: none;
+    padding-right: 0;
+    padding-left: 0;
+  }
+</style>
--- test/unit/specs/components/atoms/KbnButton.spec.js
+++ test/unit/specs/components/atoms/KbnButton.spec.js
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import KbnButton from '@components/atoms/KbnButton.vue'
+import KbnButton from '@/components/atoms/KbnButton.vue'
 
 describe('KbnButton', () => {
   describe('プロパティ', () => {
@@ -20,6 +20,62 @@ describe('KbnButton', () => {
           expect(button.classes()).to.include('kbn-button')
         })
       })
+      describe('text', () => {
+        it('kbn-button-textクラスを持つbutton要素で構成されること', () => {
+          const button = mount(KbnButton, {
+            propsData: {type: 'text'}
+          })
+          expect(button.is('button')).to.equal(true)
+          expect(button.classes()).to.include('kbn-button-text')
+        })
+      })
+      describe('disabled', () => {
+        it('disabled属性が付与されていないこと', () => {
+          const button = mount(KbnButton)
+          expect(button.attributes().disabled).to.be.an('undefined')
+        })
+      })
+      describe('true', () => {
+        it('disabled属性が付与されていること', () => {
+          const button = mount(KbnButton, {
+            propsData: { disabled: true }
+          })
+          expect(button.attributes().disabled).to.equal('disabled')
+        })
+      })
+      describe('false', () => {
+        it('disabled属性が付与されていないこと', () => {
+          const button = mount(KbnButton)
+          expect(button.attributes().disabled).to.be.an('undefined')
+        })
+      })
+    })
+  })
+})
+
+describe('イベント', () => {
+  describe('click', () => {
+    it('発行されていること', () => {
+      const button = mount(KbnButton)
+      button.trigger('click')
+      expect(button.emitted().click.length).to.equals(1)
+    })
+  })
+})
+
+describe('スロット', () => {
+  describe('コンテンツ挿入あり', () => {
+    it('挿入されていること', () => {
+      const button = mount(KbnButton, {
+        slots: { default: '<p>hello</p>' }
+      })
+      expect(button.text()).equals('hello')
+    })
+  })
+  describe('コンテンツ挿入なし', () => {
+    it('挿入されていないこと', () => {
+      const button = mount(KbnButton)
+      expect(button.text()).equals('')
     })
   })
 })
