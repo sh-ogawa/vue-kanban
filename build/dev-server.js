18e78e02c146c218c08c6aa956cc5d50e2986848
sh-ogawa <syogo0417@gmail.com>
2018-11-02 12:46:49 +0900

    add dev-server.

--- build/dev-server.js
+++ build/dev-server.js
@@ -3,4 +3,32 @@ const bodyParser = require('body-parser')
 module.exports = app => {
   app.use(bodyParser.json())
 
+  const users = {
+    'foo@domain.com': {
+      password: '12345678',
+      userId: 1,
+      token: '1234567890abcdef'
+    }
+  }
+
+  app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body
+    const user = users[email]
+    if (user) {
+      if(user.password === password) {
+        res.json({
+          userId: user.userId,
+          token: user.token
+        })
+      } else {
+        res.status(401).json({
+          message: 'ログインに失敗しました'
+        })
+      }
+    } else {
+      res.status(404).json({
+        message: 'ユーザーが登録されていません'
+      })
+    }
+  })
 }
