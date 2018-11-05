ef60ac165bbc6c4de1c4567e873c157a1f6e65cf
sh-ogawa <syogo0417@gmail.com>
2018-11-05 23:40:42 +0900

    add e2e test

---
+++ test/e2e/specs/login.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'ログイン': function (browser) {
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 1000)
+      .enterValue('input#email', 'foo@domain.com')
+      .enterValue('input#password', '12345678')
+      .waitForElementPresent('form > .form-actions > button', 1000)
+      .click('form > .form-actions > button')
+      .waitForElementPresent('#app > p', 1000)
+      .assert.urlEquals('http://localhost:8080/#/')
+      .end()
+  }
+}
