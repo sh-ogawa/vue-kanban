ec4c12744dbab833af6ef853ac272292983183b2
sh-ogawa <syogo0417@gmail.com>
2018-11-07 12:14:54 +0900

    add global error handler.

--- src/main.js
+++ src/main.js
@@ -11,6 +11,12 @@ Vue.config.productionTip = false
 
 Vue.component(ErrorBoundary.name, ErrorBoundary)
 
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error('errorHandler err:', err)
+  console.error('errorHandler vm:', vm)
+  console.error('errorHandler info:', info)
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
